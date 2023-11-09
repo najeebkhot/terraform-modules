b1aca1b5bd4e69e493f6aa08f40d5ae1b590ad85
najeebkhot <najeebkhot.1@gmail.com>
2023-11-09 14:21:37 +0530

    Made changes to subnet file

--- subnet.tf
+++ subnet.tf
@@ -1,5 +1,5 @@
 resource "aws_subnet" "subnet" {
-    availability_zone = element(data.aws_availability_zones.available_zones.names, count.index)
+    availability_zone = element(var.azs, count.index)
     vpc_id     = aws_vpc.vpc.id
     cidr_block = [var.subnet_cidr_block]
 }
