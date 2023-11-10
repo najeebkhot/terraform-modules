fc170ad59efece4f65a3f6b275d756118fc5876d
najeebkhot <najeebkhot.1@gmail.com>
2023-11-10 16:11:55 +0530

    Added resource block for private and public subnet.

--- subnet.tf
+++ subnet.tf
@@ -1,5 +1,11 @@
-resource "aws_subnet" "subnet" {
-    availability_zone = element(var.azs, count.index)
-    vpc_id     = aws_vpc.vpc.id
+resource "aws_subnet" "public_subnet" {
+    availability_zone = [var.availability_zone]
+    vpc_id     = var.vpc_id
+    cidr_block = [var.subnet_cidr_block]
+}
+
+resource "aws_subnet" "private_subnet" {
+    availability_zone = [var.availability_zone]
+    vpc_id     = var.vpc_id
     cidr_block = [var.subnet_cidr_block]
 }
